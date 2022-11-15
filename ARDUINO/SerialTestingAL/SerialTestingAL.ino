eda5affe17fb6aff689c6b2e805be02206afd61d
jadwin1997 <jadwinmanuel@gmail.com>
2022-11-15 16:14:03 -0600

    Add UART arduino script

---
+++ ARDUINO/SerialTestingAL/SerialTestingAL.ino
@@ -0,0 +1,21 @@
+int mode = 0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+pinMode(LED_BUILTIN,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()>0){
+  mode = int(Serial.read());
+  //Serial.println(mode);
+}
+if(mode == 1){
+  digitalWrite(LED_BUILTIN,HIGH);
+  
+}
+else{
+  digitalWrite(LED_BUILTIN,LOW);
+}
+}
