c2e8e10424baa822b5914e011ec4729c2062e4db
jadwin1997 <jadwinmanuel@gmail.com>
2022-11-26 21:43:17 -0600

    Update SerialTestingAL.ino

--- ARDUINO/SerialTestingAL/SerialTestingAL.ino
+++ ARDUINO/SerialTestingAL/SerialTestingAL.ino
@@ -1,4 +1,7 @@
+//This 
+
 int mode = 0;
+String data;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);
@@ -9,6 +12,8 @@ void loop() {
   // put your main code here, to run repeatedly:
 if(Serial.available()>0){
   mode = int(Serial.read());
+  Serial.println(mode);
+ 
   //Serial.println(mode);
 }
 if(mode == 1){
