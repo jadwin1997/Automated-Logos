4e7fd235978a88a84d8646247ffa033100f32eab
jadwin1997 <jadwinmanuel@gmail.com>
2023-04-25 18:14:42 -0500

    Update riorand_nonblocking.ino

--- ARDUINO/EXAMPLES/riorand_nonblocking/riorand_nonblocking.ino
+++ ARDUINO/EXAMPLES/riorand_nonblocking/riorand_nonblocking.ino
@@ -24,104 +24,10 @@ volatile unsigned long pulseWidth2;
 //PWM INPUT PINS FROM PIXHAWK
 const int pwmPin1 = 3;
 const int pwmPin2 = 9;
-
-
-void setup() 
-{
-    // Set DIR pin to output
-    pinMode(PIN_DIR1, OUTPUT);
-    
-    // Set PWM pin to output
-    pinMode(PIN_PWM1, OUTPUT);
-    pinMode(pwmPin1,INPUT);
-
-    // Set DIR pin to output
-    pinMode(PIN_DIR2, OUTPUT);
-    
-    // Set PWM pin to output
-    pinMode(PIN_PWM2, OUTPUT);
-    pinMode(pwmPin2,INPUT);
-
-
-    
-    Serial.begin(9600);
-      attachInterrupt(digitalPinToInterrupt(pwmPin1), handleInterrupt1, CHANGE);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      attachInterrupt(digitalPinToInterrupt(pwmPin2), handleInterrupt1, CHANGE);
-=======
-      attachInterrupt(digitalPinToInterrupt(pwmPin2), handleInterrupt2, CHANGE);
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-=======
-      attachInterrupt(digitalPinToInterrupt(pwmPin2), handleInterrupt2, CHANGE);
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-}
-
-void loop() 
-{
-    
-    //motor 1
-    if(pulseWidth1 > 1500){
-      digitalWrite(PIN_DIR1,HIGH);
-      analogWrite(PIN_PWM1, map(pulseWidth1,1500,2000,0,255));
-<<<<<<< HEAD
-<<<<<<< HEAD
-      Serial.print(pulseWidth1);
-=======
-      Serial.print(PIN_PWM1);
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-=======
-      Serial.print(PIN_PWM1);
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-      Serial.println(" Forward");
-    }
-    else{
-      digitalWrite(PIN_DIR1,LOW);
-      analogWrite(PIN_PWM1, map(pulseWidth1,1000,1500,255,0));
-      Serial.print(pulseWidth1);
-      Serial.println(" Backward");
-    }
-
-    
-    //motor 2
-    if(pulseWidth2 > 1500){
-      digitalWrite(PIN_DIR2,HIGH);
-      analogWrite(PIN_PWM2, map(pulseWidth2,1500,2000,0,255));
-      Serial.print(pulseWidth2);
-      Serial.println(" Forward");
-    }
-    else{
-      digitalWrite(PIN_DIR2,LOW);
-<<<<<<< HEAD
-<<<<<<< HEAD
-      analogWrite(PIN_PWM2, map(pulseWidth2,1000,1500,255,0));
-=======
-      analogWrite(PIN_PWM2, map(pulseWidth2,1000,1500,0,255));
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-=======
-      analogWrite(PIN_PWM2, map(pulseWidth2,1000,1500,0,255));
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-      Serial.print(pulseWidth2);
-      Serial.println(" Backward");
-    }
-
-    
-}
-
-
-
 void handleInterrupt1() {
   static unsigned long start_time;
   static bool state = LOW;
-<<<<<<< HEAD
-<<<<<<< HEAD
   handleInterrupt2();
-=======
-  
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
-=======
-  
->>>>>>> dad92f1aa9cfe76f52e2e5c645a21eb9644ae634
   if (digitalRead(pwmPin1) == state) {
     // This is a glitch in the signal, ignore it
     return;
@@ -157,4 +63,62 @@ void handleInterrupt2() {
   }
   
   state = !state;
+}
+
+void setup() 
+{
+    // Set DIR pin to output
+    pinMode(PIN_DIR1, OUTPUT);
+    
+    // Set PWM pin to output
+    pinMode(PIN_PWM1, OUTPUT);
+    pinMode(pwmPin1,INPUT);
+
+    // Set DIR pin to output
+    pinMode(PIN_DIR2, OUTPUT);
+    
+    // Set PWM pin to output
+    pinMode(PIN_PWM2, OUTPUT);
+    pinMode(pwmPin2,INPUT);
+
+
+    
+    Serial.begin(9600);
+      attachInterrupt(digitalPinToInterrupt(pwmPin1), handleInterrupt1, CHANGE);
+      attachInterrupt(digitalPinToInterrupt(pwmPin2), handleInterrupt1, CHANGE);
+
+}
+
+void loop() 
+{
+    
+    //motor 1
+    if(pulseWidth1 > 1500){
+      digitalWrite(PIN_DIR1,HIGH);
+      analogWrite(PIN_PWM1, map(pulseWidth1,1500,2000,0,255));
+      Serial.print(pulseWidth1);
+    }
+    else{
+      digitalWrite(PIN_DIR1,LOW);
+      analogWrite(PIN_PWM1, map(pulseWidth1,1000,1500,255,0));
+      Serial.print(pulseWidth1);
+      Serial.println(" Backward");
+    }
+
+    
+    //motor 2
+    if(pulseWidth2 > 1500){
+      digitalWrite(PIN_DIR2,HIGH);
+      analogWrite(PIN_PWM2, map(pulseWidth2,1500,2000,0,255));
+      Serial.print(pulseWidth2);
+      Serial.println(" Forward");
+    }
+    else{
+      digitalWrite(PIN_DIR2,LOW);
+      analogWrite(PIN_PWM2, map(pulseWidth2,1000,1500,255,0));
+      Serial.print(pulseWidth2);
+      Serial.println(" Backward");
+    }
+
+    
 }
